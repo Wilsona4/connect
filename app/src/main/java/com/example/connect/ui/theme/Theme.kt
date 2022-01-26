9c3bf39cc20cee4d9acd536b277282423ba75549
Mac <mac@Wils-MacBook-Pro.local>
2022-01-26 19:02:04 +0100

    Implement Splash Screen

---
+++ app/src/main/java/com/example/connect/ui/theme/Theme.kt
@@ -0,0 +1,47 @@
+package com.example.connect.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = GreenAccent,
+    background = DarkGray,
+    onBackground = TextWhite,
+    onPrimary = DarkGray,
+    surface = MediumGray,
+    onSurface = LightGray
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun ConnectTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+//    val colors = if (darkTheme) {
+//        DarkColorPalette
+//    } else {
+//        LightColorPalette
+//    }
+
+    MaterialTheme(
+        colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
